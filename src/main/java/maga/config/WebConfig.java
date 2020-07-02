b92d267b371d90ae2f4970237ed94da9e7ba8340
mustafaag <mustafaaga23@gmail.com>
2020-07-02 16:25:49 +0200

    model fix

--- src/main/java/maga/config/WebConfig.java
+++ src/main/java/maga/config/WebConfig.java
@@ -1,8 +1,11 @@
 package maga.config;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 
 @EnableWebMvc
@@ -11,4 +14,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 public class WebConfig {
 
+    // == constants ==
+    public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
+    public static final String RESOLVER_SUFFIX = ".jsp";
+
+    // == bean methods
+    @Bean
+    public ViewResolver viewResolver() {
+        UrlBasedViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix(RESOLVER_PREFIX);
+        viewResolver.setSuffix(RESOLVER_SUFFIX);
+        return viewResolver;
+    }
 }
--- src/main/java/maga/controller/DemoController.java
+++ src/main/java/maga/controller/DemoController.java
@@ -1,16 +1,37 @@
 package maga.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
-@RequestMapping("todo-list")
 public class DemoController {
 
     // http://localhost:8080/todo-list/hello
+    @ResponseBody
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello";
+    public String hello() {
+        return "hello";
+    }
+
+    // http://localhost:8080/todo-list/welcome
+    @GetMapping("welcome")
+    public String welcome(Model model) {
+        model.addAttribute("user", "Mustafa");
+        log.info("model= {}", model);
+
+        // prefix + name + suffix
+        // /WEB-INF/view/welcome.jsp
+        return "welcome";
+    }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage() {
+        log.info("welcomeMessage() called");
+        return "Welcome to this Demo application.";
     }
 }
